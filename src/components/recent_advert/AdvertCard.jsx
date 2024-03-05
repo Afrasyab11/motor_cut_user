1942fcbdad1d00b59c594521f6c2d6685436d8fe
Muhammad Afrasyab <makust7@gmail.com>
2024-03-05 10:49:49 +0500

    fixed the responsive issue in create advert

--- src/components/recent_advert/AdvertCard.jsx
+++ src/components/recent_advert/AdvertCard.jsx
@@ -93,66 +93,66 @@ export default function AdvertCard({ data, showCard }) {
             className="lg:grid lg:grid-cols-12 md:grid md:grid-cols-12 sm:gird sm:grid-cols-12 bg-whitee gap-3 gap-x-5 gap-y-2 p-4 rounded-2xl mb-5 shadow-xl"
           >
             {/* Image Section */}
-            <div className="lg:col-span-5 md:col-span-12 mb-2">
+            <div className="lg:col-span-5 md:col-span-12 mb-1">
               <Image
                 src={`${baseDomain}get-file?filename=${item?.Images?.Images[0]?.Original}`}
                 alt={"advert"}
                 width={1600}
                 height={900}
                 // className="w-full h-full object-cover rounded-2xl"
-                className="w-full min-h-[150px] max-h-[150px] object-contain rounded-2xl"
+                className="w-full min-h-[120px] max-h-[120px] object-contain rounded-2xl"
               />
             </div>
             {/* Details Section */}
             <div className="lg:col-span-7  md:col-span-12 mb-2">
               <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 3xl:grid-cols-2 gap-2 md:gap-y-2 lg:gap-y-1 xl:gap-y-1 2xl:gap-y-1 3xl:gap-y-2 mt-2 sm:mt-2 mb-1 pt-1 lg:m-0">
                 <div>
-                  <span className="text-sm sm:text-md lg:text-[13px] xl:text-[15px] 2xl:text-[20px]">
+                  <span className="text-sm sm:text-md lg:text-[13px] xl:text-[13px] 2xl:text-[20px]">
                     Label:
                   </span>
                 </div>
                 <div>
-                  <p className="text-sm sm:text-md truncate lg:text-[13px] xl:text-[15px] 2xl:text-[20px]">
+                  <p className="text-sm sm:text-md truncate lg:text-[13px] xl:text-[13px] 2xl:text-[20px]">
                     {item?.Label}
                   </p>
                 </div>
                 <div>
-                  <span className="text-sm sm:text-md lg:text-[13px] xl:text-[15px] 2xl:text-[20px]">
+                  <span className="text-sm sm:text-md lg:text-[13px] xl:text-[13px] 2xl:text-[20px]">
                     Cut Type:
                   </span>
                 </div>
                 <div>
-                  <p className="text-sm sm:text-md truncate lg:text-[13px] xl:text-[15px] 2xl:text-[20px]">
+                  <p className="text-sm sm:text-md truncate lg:text-[13px] xl:text-[13px] 2xl:text-[20px]">
                     {item?.CutType}
                   </p>
                 </div>
                 <div>
-                  <p className="text-sm sm:text-md truncate lg:text-[13px] xl:text-[15px] 2xl:text-[20px]">
+                  <p className="text-sm sm:text-md truncate lg:text-[13px] xl:text-[13px] 2xl:text-[20px]">
                     Date:
                   </p>
                 </div>
                 <div>
-                  <p className="text-sm sm:text-md truncate lg:text-[13px] xl:text-[15px] 2xl:text-[20px]">
+                  <p className="text-sm sm:text-md truncate lg:text-[13px] xl:text-[13px] 2xl:text-[20px]">
                     {item?.Date}
                   </p>
                 </div>
                 <div>
-                  <span className="text-sm sm:text-md lg:text-[13px] xl:text-[15px] 2xl:text-[20px]">
+                  <span className="text-sm sm:text-md lg:text-[13px] xl:text-[13px] 2xl:text-[20px]">
                     Time:
                   </span>
                 </div>
                 <div>
-                  <span className="text-sm sm:text-md lg:text-[13px] xl:text-[15px] 2xl:text-[20px]">
+                  <span className="text-sm sm:text-md lg:text-[13px] xl:text-[13px] 2xl:text-[20px]">
                     {item?.Time}
                   </span>
                 </div>
                 <div>
-                  <p className="text-sm sm:text-md truncate lg:text-[13px] xl:text-[15px] 2xl:text-[20px]">
+                  <p className="text-sm sm:text-md truncate lg:text-[13px] xl:text-[13px] 2xl:text-[20px]">
                     No of Images:
                   </p>
                 </div>
                 <div>
-                  <span className="text-sm sm:text-md lg:text-[13px] xl:text-[15px] 2xl:text-[20px]">
+                  <span className="text-sm sm:text-md lg:text-[13px] xl:text-[13px] 2xl:text-[20px]">
                     {item.Images?.Images?.length}
                   </span>
                 </div>
@@ -162,7 +162,7 @@ export default function AdvertCard({ data, showCard }) {
             <div className="lg:col-span-5 md:col-span-12 sm:col-span-12 flex justify-center mb-1  md:m-0">
               <div className="lg:grid-cols-12 md:grid-cols-12 sm:grid-cols-12 w-full lg:w-32">
                 <p
-                  className={` col-span-4 rounded-full w-full text-center text-sm sm:text-md lg:text-[13px] xl:text-[15px] 2xl:text-[20px] py-1 lg:py-1 xl:py-2 2xl:py-2 3xl:py-2 m-0 ${
+                  className={` col-span-4 rounded-full w-full text-center text-sm sm:text-md lg:text-[13px] xl:text-[13px] 2xl:text-[20px] py-1 lg:py-1 xl:py-1 2xl:py-2 3xl:py-2 m-0 ${
                     item?.Status == "Completed"
                       ? "bg-site_green text-whitee"
                       : "bg-site_orange text-primary[dark]"
@@ -182,7 +182,7 @@ export default function AdvertCard({ data, showCard }) {
                       query: { advertId: item.UniqueAdvertisementId },
                     }}
                   >
-                    <button className="text-primary border w-full rounded-full  py-1  lg:py-1 xl:py-2 2xl:py-2 3xl:py-2 text-sm sm:text-md lg:text-[13px] xl:text-[15px] 2xl:text-[20px]">
+                    <button className="text-primary border w-full rounded-full  py-1  lg:py-1 xl:py-1 2xl:py-2 3xl:py-2 text-sm sm:text-md lg:text-[13px] xl:text-[13px] 2xl:text-[20px]">
                       View Images
                     </button>
                   </Link>
@@ -190,7 +190,7 @@ export default function AdvertCard({ data, showCard }) {
                 <div className="lg:col-span-6 md:col-span-12  sm:col-span-12">
                   <button
                     onClick={(e) => downloadImagesHandler(e, item)}
-                    className="text-whitee bg-primary  w-full rounded-full  py-1 lg:py-1 xl:py-2 2xl:py-2 3xl:py-2 text-sm sm:text-md lg:text-[13px] xl:text-[15px] 2xl:text-[20px]"
+                    className="text-whitee bg-primary  w-full rounded-full  py-1 lg:py-1 xl:py-1 2xl:py-2 3xl:py-2 text-sm sm:text-md lg:text-[13px] xl:text-[13px] 2xl:text-[20px]"
                   >
                     Download
                   </button>
