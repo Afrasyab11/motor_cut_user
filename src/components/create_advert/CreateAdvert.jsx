ce88b6f4411167acb4fc7de472d30f78c342074b
Muhammad Afrasyab <makust7@gmail.com>
2024-03-09 11:15:52 +0500

    Fixed the Background Size by Gulfam at 11:15AM 9March

--- src/components/create_advert/CreateAdvert.jsx
+++ src/components/create_advert/CreateAdvert.jsx
@@ -146,14 +146,7 @@ export default function CreateAdvert() {
           </div>
         </div>
         <div className="mt-6 flex justify-center">
-          {/* <Image
-            className="w-full lg:h-[200px] rounded-3xl mx-2 overflow-hidden"
-            src={`${baseDomain}get-file?filename=${background?.BackgroundImage}`}
-            objectFit="cover"
-            height={30}
-            width={30}
-            alt=""
-          /> */}
+          
           <div className="rounded-2xl">
             <Image
               src={
