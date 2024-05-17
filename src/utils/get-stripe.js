4f2a180828acb07e18d7922bc6f310e6c03c337f
Muhammad Afrasyab <afrasyab.cortechsols@gmail.com>
2024-05-17 11:20:13 +0500

    updated the strip key's in one page

--- src/utils/get-stripe.js
+++ src/utils/get-stripe.js
@@ -1,25 +1,23 @@
 import {  loadStripe } from '@stripe/stripe-js';
 
-// let stripePromise;
-// const getStripe = () => {
-//     if (!stripePromise) {
-//             // process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-
-//         stripePromise = loadStripe(
-//             "pk_test_51OUlCAE66tYGrLUMDWjRkd0wfZOSrjcZWGRbvRFzQ2Dk4JK478haIldZxltBMsYoIhuALDTYzMbO9rekvZXAok1F00Y4EeXCzB"
-//             );
-//     }
-//     return stripePromise;
-// };
-
 let stripePromise;
 const getStripe = () => {
     if (!stripePromise) {
-        // const publishableKey = process.env.STRIPE_PUBLIC_KEY;
-        // const publishableKey = `${process.env.PUBLISHABLE_KEY}`
-        stripePromise = loadStripe(`${process.env.STRIPE_PUBLIC_KEY}`);
+            // process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+
+        stripePromise = loadStripe(
+            "pk_test_51OUlCAE66tYGrLUMDWjRkd0wfZOSrjcZWGRbvRFzQ2Dk4JK478haIldZxltBMsYoIhuALDTYzMbO9rekvZXAok1F00Y4EeXCzB"
+            );
     }
     return stripePromise;
 };
+
+// let stripePromise;
+// const getStripe = () => {
+//     if (!stripePromise) {
+//         stripePromise = loadStripe(`${process.env.STRIPE_PUBLIC_KEY}`);
+//     }
+//     return stripePromise;
+// };
 
 export default getStripe;
